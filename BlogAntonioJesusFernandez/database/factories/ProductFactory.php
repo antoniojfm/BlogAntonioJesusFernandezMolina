166288ecb1293bb895a5062db78b99fceedae0a1
antoniojfm <91548202+antoniojfm@users.noreply.github.com>
2023-02-09 17:29:46 +0100

    Subida de archivos

---
+++ BlogAntonioJesusFernandez/database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->text(100),
+            'description' => $this->faker->text(100),
+            'quantity' => $this->faker->numberBetween(1,20),
+            'status' => $this->faker->numberBetween(1,20),
+            'seller_id' => Comment::factory(),
+        ];
+    }
+}
---
+++ BlogAntonioJesusFernandez/database/migrations/2023_02_08_115112_add_commentid_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCommentidToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('comment_id')->references('id')->on('comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('comment_id');
+        });
+    }
+}
